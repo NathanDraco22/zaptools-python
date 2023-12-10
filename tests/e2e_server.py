3d2bb8c2ffcfa7e15ebff1885cb1d0b279ad2e2b
NathanDraco22 <nathandraco22@gmail.com>
2023-12-09 21:07:20 -0600

    coroutines added

--- tests/e2e_server.py
+++ tests/e2e_server.py
@@ -3,39 +3,41 @@ from zaptools.tools import EventRegister, EventContext
 from zaptools.connectors import FastApiConnector
 
 app:FastAPI = FastAPI()
-register: EventRegister = EventRegister()
+reg: EventRegister = EventRegister()
 
-@register.on_event("connected")
+@reg.on_event("connected")
 async def connected_trigger(ctx: EventContext):
-    ctx.connection.send("connected", "LIVE", {"myHeader": "I'm a header"})
+    await ctx.connection.send("connected", "LIVE", {"myHeader": "I'm a header"})
 
-@register.on_event("disconnected")
+@reg.on_event("disconnected")
 async def disconnected_trigger(ctx:EventContext):
     print(f"connection left -> {ctx.connection.id}")
 
-@register.on_event("header")
+@reg.on_event("header")
 async def headers( ctx:EventContext):
     test_header = ctx.headers["clientHeader"]
-    ctx.connection.send("headerTest", "headerTest", {"isOk" : test_header=="client"})
+    await ctx.connection.send(
+        "headerTest", "headerTest", {"isOk" : test_header=="client"}
+)
 
-@register.on_event("event1")
+@reg.on_event("event1")
 async def event1_triger(ctx: EventContext):
-    ctx.connection.send("event1_completed", "HELLO FROM SERVER")
+    await ctx.connection.send("event1_completed", "HELLO FROM SERVER")
 
-@register.on_event("event2")
+@reg.on_event("event2")
 async def event2_triger(ctx: EventContext):
-    ctx.connection.send("event2_completed", "HELLO FROM SERVER 2")
+    await ctx.connection.send("event2_completed", "HELLO FROM SERVER 2")
 
-@register.on_event("exit")
+@reg.on_event("exit")
 async def exit_event( ctx: EventContext ):
-    ctx.connection.close()
+    await ctx.connection.close()
 
-@register.on_event("hb")
+@reg.on_event("hb")
 async def hello_and_bye(ctx:EventContext):
     conn = ctx.connection
-    conn.send("h", "h event")
-    conn.close()
+    await conn.send("h", "h event")
+    await conn.close()
 
 @app.websocket("/")
 async def websocket_endpoint(ws: WebSocket):
-    await FastApiConnector.plug_and_start(register,ws)
+    await FastApiConnector.plug_and_start(reg,ws)
