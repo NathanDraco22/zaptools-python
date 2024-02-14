5563ae3b107db993667d41c8720b4680453e86ed
NathanDraco22 <nathandraco22@gmail.com>
2024-02-13 19:44:05 -0600

    connection id added

--- zaptools/connectors.py
+++ zaptools/connectors.py
@@ -10,16 +10,22 @@ from .adapters import FastApiAdapter, SanicAdapter
 
 class FastApiConnector:
 
-    def __init__(self, register: EventRegister, webscoket: Any):
+    def __init__(
+            self, 
+            register: EventRegister, 
+            webscoket: Any, 
+            connection_id:str|None=None
+    ):
         self.register = register
         self.websocket = webscoket
+        self._connection_id = connection_id
 
     async def start(self):
         fastapi_adapter = FastApiAdapter(self.websocket)
         event_caller = EventCaller(self.register._event_book)
         id_controller = IDController()
         await fastapi_adapter.start_connection()
-        current_id = id_controller.eval()
+        current_id = id_controller.eval(self._connection_id)
         ws_connection = WebSocketConnection(current_id, fastapi_adapter)
         event_processor = EventProcessor(ws_connection, event_caller)
         await event_processor.start_event_stream()
@@ -49,16 +55,22 @@ class FastApiConnector:
 
 class SanicConnector:
 
-    def __init__(self, register: EventRegister, webscoket: Any):
+    def __init__(
+            self, 
+            register: EventRegister, 
+            webscoket: Any,
+            connection_id:str|None=None
+    ):
         self.register = register
         self.websocket = webscoket
+        self.connection_id = connection_id
 
     async def start(self):
         sanic_adapter = SanicAdapter(self.websocket)
         event_caller = EventCaller(self.register._event_book)
         id_controller = IDController()
         await sanic_adapter.start_connection()
-        current_id = id_controller.eval()
+        current_id = id_controller.eval(self.connection_id)
         ws_connection = WebSocketConnection(current_id, sanic_adapter)
         event_processor =  EventProcessor(ws_connection, event_caller)
         await event_processor.start_event_stream()
