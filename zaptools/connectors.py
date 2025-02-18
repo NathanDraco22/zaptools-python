9ca106000fa8317fbd8f37043d02e212611285d1
NathanDraco22 <33228327+NathanDraco22@users.noreply.github.com>
2025-02-17 21:20:59 -0600

    Version 0 3 0 (#12)
    * added websockets dependency
    
    * client and logger added
    
    * documentos updated
    
    * added error logger
    
    * added error handlers
    
    * added version 0-4-0
    
    * formatter fixed

--- zaptools/connectors.py
+++ zaptools/connectors.py
@@ -1,23 +1,21 @@
 from typing import Any
 from .tools import (
-    EventRegister, 
-    EventCaller, 
-    IDController, 
+    EventRegister,
+    EventCaller,
+    IDController,
     WebSocketConnection,
-    EventProcessor
+    EventProcessor,
 )
 from .adapters import FastApiAdapter, SanicAdapter
+
 
 class FastApiConnector:
 
     def __init__(
-            self, 
-            register: EventRegister, 
-            webscoket: Any, 
-            connection_id:str|None=None
+        self, register: EventRegister, websocket: Any, connection_id: str | None = None
     ):
         self.register = register
-        self.websocket = webscoket
+        self.websocket = websocket
         self._connection_id = connection_id
 
     async def start(self):
@@ -30,7 +28,6 @@ class FastApiConnector:
         event_processor = EventProcessor(ws_connection, event_caller)
         await event_processor.start_event_stream()
 
-
     @staticmethod
     async def plug(register: EventRegister, websocket: Any):
         fastapi_adapter = FastApiAdapter(websocket)
@@ -40,7 +37,7 @@ class FastApiConnector:
         current_id = id_controller.eval()
         ws_connection = WebSocketConnection(current_id, fastapi_adapter)
         return EventProcessor(ws_connection, event_caller)
-    
+
     @staticmethod
     async def plug_and_start(register: EventRegister, websocket: Any):
         fastapi_adapter = FastApiAdapter(websocket)
@@ -56,13 +53,10 @@ class FastApiConnector:
 class SanicConnector:
 
     def __init__(
-            self, 
-            register: EventRegister, 
-            webscoket: Any,
-            connection_id:str|None=None
+        self, register: EventRegister, websocket: Any, connection_id: str | None = None
     ):
         self.register = register
-        self.websocket = webscoket
+        self.websocket = websocket
         self.connection_id = connection_id
 
     async def start(self):
@@ -72,7 +66,7 @@ class SanicConnector:
         await sanic_adapter.start_connection()
         current_id = id_controller.eval(self.connection_id)
         ws_connection = WebSocketConnection(current_id, sanic_adapter)
-        event_processor =  EventProcessor(ws_connection, event_caller)
+        event_processor = EventProcessor(ws_connection, event_caller)
         await event_processor.start_event_stream()
 
     @staticmethod
@@ -84,7 +78,7 @@ class SanicConnector:
         current_id = id_controller.eval()
         ws_connection = WebSocketConnection(current_id, fastapi_adapter)
         return EventProcessor(ws_connection, event_caller)
-    
+
     @staticmethod
     async def plug_and_start(register: EventRegister, websocket: Any):
         fastapi_adapter = SanicAdapter(websocket)
@@ -93,5 +87,5 @@ class SanicConnector:
         await fastapi_adapter.start_connection()
         current_id = id_controller.eval()
         ws_connection = WebSocketConnection(current_id, fastapi_adapter)
-        event_processor =  EventProcessor(ws_connection, event_caller)
+        event_processor = EventProcessor(ws_connection, event_caller)
         await event_processor.start_event_stream()
