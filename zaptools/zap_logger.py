be4008bede71cfcb5c34b746fa980e46b3713b20
NathanDraco22 <nathandraco22@gmail.com>
2025-02-22 21:06:41 -0600

    fix logger deplicated

--- zaptools/zap_logger.py
+++ zaptools/zap_logger.py
@@ -2,29 +2,29 @@ import logging
 from rich.logging import RichHandler
 
 
-FORMAT = "%(message)s"
-class ZapLogger():
+class ZapLogger:
     def __init__(self):
-        logging.basicConfig(
-             level= logging.INFO, 
-             format=FORMAT, datefmt="[%X]", 
-             handlers=[RichHandler(rich_tracebacks=True)]
+        self.logger = logging.getLogger("zap_logger")
+        self.logger.addHandler(
+            RichHandler(
+                markup=True,
+                rich_tracebacks=True,
+                show_path=False,
+            )
         )
-        self.logger = logging.getLogger('zap_logger')
-    
+        self.logger.setLevel(logging.INFO)
+
     def info(self, message):
         self.logger.info(f"[Zap]{message}")
-    
+
     def info_green(self, message):
         self.logger.info(f"[green][Zap]{message}", extra={"markup": True})
-    
+
     def warning(self, message):
         self.logger.warning(f"[yellow][Zap]{message}", extra={"markup": True})
-    
+
     def error(self, message):
         self.logger.error(f"[red][Zap]{message}", extra={"markup": True})
 
 
 zap_logger = ZapLogger()
-
-
