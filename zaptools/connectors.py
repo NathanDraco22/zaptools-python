3b219580d099cd9e62698393427f584a4d8057ba
NathanDraco22 <nathandraco22@gmail.com>
2025-04-17 16:08:26 -0600

    removed unncessary static method

--- zaptools/connectors.py
+++ zaptools/connectors.py
@@ -29,7 +29,7 @@ class FastApiConnector:
         self.websocket = websocket
         self._connection_id = connection_id
 
-    async def start(self):
+    async def start(self) -> None:
         fastapi_adapter = FastApiAdapter(self.websocket)
         event_caller = EventCaller(self.register._event_book)  # type: ignore
         id_controller = IDController()
@@ -37,33 +37,6 @@ class FastApiConnector:
         await fastapi_adapter.start_connection()
 
         current_id = id_controller.eval(self._connection_id)
-        ws_connection = WebSocketConnection(current_id, fastapi_adapter)
-        event_processor = EventProcessor(ws_connection, event_caller)
-
-        await event_processor.start_event_stream()
-
-    @staticmethod
-    async def plug(register: EventRegister, websocket: Any):
-        fastapi_adapter = FastApiAdapter(websocket)
-        event_caller = EventCaller(register._event_book)  # type: ignore
-        id_controller = IDController()
-
-        await fastapi_adapter.start_connection()
-
-        current_id = id_controller.eval()
-        ws_connection = WebSocketConnection(current_id, fastapi_adapter)
-
-        return EventProcessor(ws_connection, event_caller)
-
-    @staticmethod
-    async def plug_and_start(register: EventRegister, websocket: Any):
-        fastapi_adapter = FastApiAdapter(websocket)
-        event_caller = EventCaller(register._event_book)  # type: ignore
-        id_controller = IDController()
-
-        await fastapi_adapter.start_connection()
-
-        current_id = id_controller.eval()
         ws_connection = WebSocketConnection(current_id, fastapi_adapter)
         event_processor = EventProcessor(ws_connection, event_caller)
 
@@ -89,7 +62,7 @@ class SanicConnector:
         self.websocket = websocket
         self.connection_id = connection_id
 
-    async def start(self):
+    async def start(self) -> None:
         sanic_adapter = SanicAdapter(self.websocket)
         event_caller = EventCaller(self.register._event_book)  # type: ignore
         id_controller = IDController()
@@ -97,33 +70,6 @@ class SanicConnector:
         await sanic_adapter.start_connection()
 
         current_id = id_controller.eval(self.connection_id)
-        ws_connection = WebSocketConnection(current_id, sanic_adapter)
-        event_processor = EventProcessor(ws_connection, event_caller)
-
-        await event_processor.start_event_stream()
-
-    @staticmethod
-    async def plug(register: EventRegister, websocket: Any):
-        sanic_adapter = SanicAdapter(websocket)
-        event_caller = EventCaller(register._event_book)  # type: ignore
-        id_controller = IDController()
-
-        await sanic_adapter.start_connection()
-
-        current_id = id_controller.eval()
-        ws_connection = WebSocketConnection(current_id, sanic_adapter)
-
-        return EventProcessor(ws_connection, event_caller)
-
-    @staticmethod
-    async def plug_and_start(register: EventRegister, websocket: Any):
-        sanic_adapter = SanicAdapter(websocket)
-        event_caller = EventCaller(register._event_book)  # type: ignore
-        id_controller = IDController()
-
-        await sanic_adapter.start_connection()
-
-        current_id = id_controller.eval()
         ws_connection = WebSocketConnection(current_id, sanic_adapter)
         event_processor = EventProcessor(ws_connection, event_caller)
 
